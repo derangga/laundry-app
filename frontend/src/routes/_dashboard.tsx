325998e8370b62edf028d198b7c38f8c101d4516
derangga <derangga1011@gmail.com>
2026-06-11 10:41:46 +0700

    feat(fe): change sidebar to inset

--- frontend/src/routes/_dashboard.tsx
+++ frontend/src/routes/_dashboard.tsx
@@ -18,9 +18,12 @@ function DashboardLayout() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+        <header className="flex h-16 shrink-0 items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Separator
+            orientation="vertical"
+            className="shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px mr-2 data-[orientation=vertical]:h-4"
+          />
           <BreadcrumbNav />
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
